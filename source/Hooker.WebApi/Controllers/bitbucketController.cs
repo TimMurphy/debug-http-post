89e030a280467b5c67688791be833f7c813a07ee
Tim Murphy <tim@26tp.com>
2015-09-27 05:54:13 +1000

    Add fbitbucket webhook stub

---
+++ source/Hooker.WebApi/Controllers/bitbucketController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Anotar.NLog;
+
+namespace Hooker.WebApi.Controllers
+{
+    // ReSharper disable once InconsistentNaming
+    public class bitbucketController : ApiController
+    {
+        // POST api/bitbucket
+        public void Post([FromBody]string value)
+        {
+            LogTo.Debug(() => $"{nameof(bitbucketController)}.{nameof(Post)}({nameof(value)}: {value})");
+        }
+    }
+}
